489365ff155f1244de2ed1e65cc7ba82660bccd9
Saltykov21ist2 <99250746+Saltykov21ist2@users.noreply.github.com>
2022-03-17 14:50:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,49 +1,44 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
-
 double pown(double value, uint16_t n) {
-    uint64_t i;
-    double y = 1;
-    for (i = 1; i <= n; i++)
-        y *= value;
-    return y;
+    uint64_t in;
+    double xs = 1;
+    for (in = 1; in <= n; in++)
+        xs *= value;
+    return xs;
 }
-
 uint64_t fact(uint16_t n) {
     if (n == 0)
         return 1;
     return fact(n-1)*n;
 }
-
-double calcItem(double y, uint16_t n) {
-    return pown(y, n)/fact(n);
+double calcItem(double xs, uint16_t n) {
+    return pown(xs, n)/fact(n);
+}
+double expn(double xs, uint16_t count) {
+    uint16_t in;
+    double d;
+    for (in = 0; in <= count; in++) {
+        d += calcItem(xs, in);
+    }
+    return d;
 }
 
-double expn(double y, uint16_t count) {
-    uint16_t i;
-    double y;
-    for (i = 0; i <= count; i++) {
-        y += calcItem(y, i);
+double sinn(double xs, uint16_t count) {
+    uint16_t in;
+    double d = 0;
+    for (in = 1; in <= count; in++) {
+        d += pown(-1, in-1)*calcItem(xs, 2*in-1);
     }
-    return y;
+    return d;
 }
 
-double sinn(double y, uint16_t count) {
-    uint16_t i;
-    double y = 0;
-    for (i = 1; i <= count; i++) {
-        y += pown(-1, i-1)*calcItem(y, 2*i-1);
+double cosn(double xs, uint16_t count) {
+    uint16_t in;
+    double d = 0;
+    for (in = 1; in <= count; in++) {
+        d += pown(-1, in-1)*calcItem(xs, 2*in-2);
     }
-    return y;
-}
-
-double cosn(double y, uint16_t count) {
-    uint16_t i;
-    double y = 0;
-    for (i = 1; i <= count; i++) {
-        y += pown(-1, i-1)*calcItem(y, 2*i-2);
-    }
-    return y;
+    return d;
 }
